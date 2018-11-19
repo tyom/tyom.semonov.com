05a76c29e980c09711dccfcc92e901308395556b
Tyom Semonov <inspiro@gmail.com>
2018-11-19 01:14:47 +0000

    Tweak list column in InfoPanelBody

--- components/IntroPanel/IntroPanelBody.vue
+++ components/IntroPanel/IntroPanelBody.vue
@@ -58,6 +58,6 @@ export default {
 .lists {
   display: grid;
   grid-gap: 1.5em;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(11em, 1fr));
 }
 </style>
