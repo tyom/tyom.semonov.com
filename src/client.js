058917b4e9c468d3d5c6dff045c691f72ae4a6bf
Tyom Semonov <inspiro@gmail.com>
2019-09-24 15:58:22 +0100

    Add GA link tracking

--- src/client.js
+++ src/client.js
@@ -1,5 +1,23 @@
 import * as sapper from '@sapper/app';
 
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 function googleAnalytics(gaId) {
   const script = document.createElement('script');
   script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
@@ -7,10 +25,25 @@ function googleAnalytics(gaId) {
 
   window.dataLayer = window.dataLayer || [];
   function gtag() {
-    dataLayer.push(arguments);
+    window.dataLayer.push(arguments);
   }
   gtag('js', new Date());
   gtag('config', gaId);
+}
+
+function trackOutboundLinkClicks(event) {
+  const [tracker] = window.ga && window.ga.getAll();
+  if (!tracker) return;
+  const anchor = event.target.closest('a');
+  if (anchor) {
+    const trackerName = tracker.a.data.values[':name'];
+    window.ga(`${trackerName}.send`, 'event', {
+      eventCategory: 'Outbound link',
+      eventAction: 'click',
+      eventLabel: anchor.href,
+      transport: 'beacon',
+    });
+  }
 }
 
 sapper.start({
@@ -19,4 +52,5 @@ sapper.start({
 
 if (process.env.GA_TRACKING_ID) {
   googleAnalytics(process.env.GA_TRACKING_ID);
+  document.addEventListener('click', trackOutboundLinkClicks);
 }
