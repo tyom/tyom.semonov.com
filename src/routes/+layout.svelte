8961979823394a5a99e6234864007ce0c5de3202
Tyom Semonov <inspiro@gmail.com>
2023-10-04 19:29:14 +0100

    Disable analytics in dev and via a manual env var setting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
+  import { dev } from '$app/environment';
+  import { PUBLIC_DISABLE_ANALYTICS } from '$env/static/public';
   import Analytics from '$components/Analytics.svelte';
   import '../global.css';
+
+  const enableAnalytics =
+    !dev && !['true', '1'].includes(PUBLIC_DISABLE_ANALYTICS);
 </script>
 
-<Analytics />
+{#if enableAnalytics}
+  <Analytics />
+{/if}
+
 <slot />
