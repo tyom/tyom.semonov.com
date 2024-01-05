2708016d281d1a56c88056baeea3d03815752f23
Tyom Semonov <inspiro@gmail.com>
2024-01-05 23:39:45 +0000

    Build PDF locally and commit it to Git
    With the latest updates the current PDF build process using Playwright is
    having issues running the server in GH Actions. Instead of building it in GH
    check in the PDF built locally.

--- playwright.config.js
+++ playwright.config.js
@@ -1,32 +1,11 @@
 import { defineConfig } from '@playwright/test';
-import { networkInterfaces } from 'os';
-
-function getIPAddress() {
-  const interfaces = networkInterfaces();
-  for (const name in interfaces) {
-    const iface = interfaces[name];
-
-    for (const alias of iface) {
-      if (
-        alias.family === 'IPv4' &&
-        alias.address !== '127.0.0.1' &&
-        !alias.internal
-      ) {
-        return alias.address;
-      }
-    }
-  }
-  return '0.0.0.0';
-}
 
 const PORT = process.env.PORT || 5173;
-const serverUrl = `http://${getIPAddress()}:${PORT}`;
-
-console.log('Server URL:', serverUrl);
+const serverUrl = `http://localhost:${PORT}`;
 
 export default defineConfig({
   webServer: {
-    command: 'pnpm run dev --host',
+    command: 'pnpm dev --host',
     url: serverUrl,
     reuseExistingServer: !process.env.CI,
   },
