4ebcbc02b085ba108e4d36afac9490570ca3e95c
Tyom Semonov <inspiro@gmail.com>
2024-01-05 22:57:37 +0000

    Use a utility function to get IP for Playwright server
    Use --host

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from '@playwright/test';
+import { networkInterfaces } from 'os';
+
+function getIPAddress() {
+  const interfaces = networkInterfaces();
+  for (const name in interfaces) {
+    const iface = interfaces[name];
+
+    for (const alias of iface) {
+      if (
+        alias.family === 'IPv4' &&
+        alias.address !== '127.0.0.1' &&
+        !alias.internal
+      ) {
+        return alias.address;
+      }
+    }
+  }
+  return '0.0.0.0';
+}
 
 const PORT = process.env.PORT || 5173;
-const serverUrl = `http://localhost:${PORT}`;
+const serverUrl = `http://${getIPAddress()}:${PORT}`;
+
+console.log('Server URL:', serverUrl);
 
 export default defineConfig({
   webServer: {
-    command: 'pnpm run dev',
+    command: 'pnpm run dev --host',
     url: serverUrl,
     reuseExistingServer: !process.env.CI,
   },
