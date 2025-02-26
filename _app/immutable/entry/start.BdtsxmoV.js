6b96311fa85d78b63296b717b822b1588e1b800d
tyom <tyom@users.noreply.github.com>
2025-02-26 10:33:34 +0000

    deploy: 517a3f716edb1e516e7639c2215c713c60c079ac

--- _app/immutable/entry/start.BdtsxmoV.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CyV4yBVn.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.BjoyBvxM.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CABXfeXr.js";export{o as load_css,r as start};
